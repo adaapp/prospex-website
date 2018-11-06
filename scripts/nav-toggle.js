797625a0866fa6bfd9436f0d308723b997346874
hkamara <hassan.kamara@ada.ac.uk>
2018-11-06 18:18:48 +0000

    Signed-off-by: hkamara <hassan.kamara@ada.ac.uk>

---
+++ scripts/nav-toggle.js
@@ -0,0 +1,9 @@
+var navToggle = document.querySelector('.navigation-box-toggle');
+var navigationBox = document.querySelector('.navigation-box');
+
+navToggle.addEventListener('click', toggleClass);
+
+function toggleClass(){
+  navigationBox.classList.toggle('closed')
+}
+$ = document.querySelector
