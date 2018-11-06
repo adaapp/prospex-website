c05311ca10f3c6040d8a427dff5d7caaff40ec5f
Unknown <edward.mccue@adacollege.org.uk>
2018-11-06 16:06:51 +0000

    menu basics

---
+++ js/nav-toggle.js
@@ -0,0 +1,8 @@
+alert('hello');
+var navToggleButton = document.querySelector('.nav-toggle');
+var navBox = document.querySelector('.nav-box');
+navToggleButton.addEventListener('click', toggleClass);
+
+function toggleClass(){
+    navBox.classList.toggle('closed')
+    }
