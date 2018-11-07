674daba6df2605721cbd69dc8ddc1180925afece
Ali Blackwell <aliblackwell@protonmail.com>
2018-11-07 19:26:21 +0000

    Add proper content

--- js/nav-toggle.js
+++ js/nav-toggle.js
@@ -2,6 +2,8 @@
 var navToggleButton = document.querySelector('.nav-toggle');
 var navBox = document.querySelector('.nav-box');
 
+var missionStatementSection = document.getElementById('mission-statement')
+var mainWrapper = document.getElementsByClassName('container-fluid')
 navToggleButton.addEventListener('click', toggleClass);
 
 function toggleClass(){
